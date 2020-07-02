29c4ffe3d6ecedda26aa2ecace36dde0d6ee9d43
Anna Trushkova <anna_trushkova.94@mail.ru>
2020-07-02 15:09:36 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import Info from './ui/Info'
+import Form from './ui/form'
+import Weather from './ui/weather'
+
+
+const API_KEY = 'f530e66e0972422a7a683543dd920af9'
+
+class App extends React.Component {
+
+    state = {
+        temp: undefined,
+        city: undefined,
+        country: undefined,
+        pressure: undefined,
+        sunset: undefined,
+        error: undefined
+    }
+
+    gettingWeather = async (e) => {
+        e.preventDefault()
+        const city = e.target.elements.city.value
+
+        if (city) {
+            const api_url = await
+                fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
+            const data = await api_url.json()
+            let sunset = data.sys.sunset
+            let date = new Date()
+            date.setTime(sunset)
+            let sunset_date = date.getHours() + ":" + (date.getMinutes() < 10 ? '0' : '') + date.getMinutes() + ":" + date.getSeconds()
+
+            this.setState({
+                temp: data.main.temp,
+                city: data.name,
+                country: data.sys.country,
+                pressure: data.main.pressure,
+                sunset: sunset_date,
+                error: undefined
+            })
+        } else {
+            this.setState({
+                temp: undefined,
+                city: undefined,
+                country: undefined,
+                pressure: undefined,
+                sunset: undefined,
+                error: "Введите название города"
+            })
+        }
+    }
+
+    render() {
+        return (
+            <div className="wrapper">
+                <div className="main">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-sm-5 info">
+                                <Info />
+                            </div>
+                            <div className="col-sm-7 form">
+                                <Form weatherMethod={this.gettingWeather} />
+                                <Weather
+                                    temp={this.state.temp}
+                                    city={this.state.city}
+                                    country={this.state.country}
+                                    pressure={this.state.pressure}
+                                    sunset={this.state.sunset}
+                                    error={this.state.error} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default App
---
+++ src/ui/weather.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../App.css'
+
+
+const Weather = (props) => {
+    return (
+        <div className="infoWeath">
+            {props.city &&
+                <div>
+                    <p>Местоположение: {props.city}, {props.country} </p>
+                    <p>Температура: {props.temp} </p>
+                    <p>Давление: {props.pressure} </p>
+                    <p>Заход Солнца: {props.sunset} </p>
+                </div>
+            }
+            <p className="error">{props.error}</p>
+        </div>
+    )
+}
+
+export default Weather
